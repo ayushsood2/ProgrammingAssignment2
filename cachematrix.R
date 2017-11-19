5c4387c63c4666f78112cab0ab46d28d8ac6a233
ayushsood2 <30906271+ayushsood2@users.noreply.github.com>
2017-11-19 10:43:43 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -14,6 +14,7 @@ makeCachematrix<- function(x=matrix()){
     x<<-y
     m<<-NULL
   }
+  get<-function() x
   setmatrix<-function(solve) m<<-solve
   getmatrix<-function() m
   list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
